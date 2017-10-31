265d8f525d7e776c72b59dbd5565a539c8d93473
Thoppay Prabakaran, Yadhunandan <yadhunandan.thoppayprabakaran@rwe.com>
2017-10-31 17:01:07 +0000

    rectified s3 copy command and validation_client_name

--- Unattended_Chef-client _bootstrap.ps1
+++ Unattended_Chef-client _bootstrap.ps1
@@ -18,8 +18,8 @@ if (-Not (Test-Path -Path $folder)){
 Write-Output "C:\chef folder created."
 
 
-Write-Output "Downloading the chef-client installable from the internet........"
 ### Download the chef-client installable
+Write-Output "Downloading the chef-client installable from the internet........"
 $chef_client_URL = "https://packages.chef.io/files/stable/chef/13.6.0/windows/2016/chef-client-13.6.0-1-x64.msi" 
 $outfile = "C:\chef-client.msi"
 Invoke-WebRequest -Uri $chef_client_URL -OutFile $outfile
@@ -56,7 +56,7 @@ Write-Output "The Node name is {0} ........" -f $node_name
 Write-Output "Creating the client.rb file........"
 $clientrb = @"
 chef_server_url             'https://ip-10-0-1-160.eu-west-2.compute.internal/organizations/rwest'
-validation_client_name      'rwest'
+validation_client_name      'rwest-validator'
 validation_key              'C:\chef\rwest-validator.pem'
 node_name                   '{0}'
 "@ -f $node_name
@@ -66,9 +66,10 @@ Write-Output "Completed, client.rb file created and placed at C:\chef."
 
 ### Place validator.pem and chef-server.crt files 
 Write-Output "Placing the required validator pem file and server SSL certificate........"
-$validator_pem_file = 'C:\chef\rwest-validator.pem'
+$validator_pem_file_local = 'C:\chef\rwest-validator.pem'
+$validator_pem_file_s3 ='s3://oracle621/chef/rwest-validator.pem'
 if (-Not (Test-Path -Path $validator_pem_file)){
-    aws s3 cp C:\chef\rwest-validator.pem s3://oracle621/chef/rwest-validator.pem
+    aws s3 cp $validator_pem_file_s3 $validator_pem_file_local
 }
 Write-Output "Completed, validator .pem file placed at C:\chef."
 
@@ -79,9 +80,10 @@ if (-Not (Test-Path -Path $folder)){
 }
 Write-Output "C:\chef\trusted_certs folder created."
 
-$certificate = 'C:\chef\trusted_certs\ip-10-0-1-160_eu-west-2_compute_internal.crt'
+$certificate_local = 'C:\chef\trusted_certs\ip-10-0-1-160_eu-west-2_compute_internal.crt'
+$certificate_s3 = 's3://oracle621/chef/ip-10-0-1-160_eu-west-2_compute_internal.crt'
 if (-Not (Test-Path -Path $certificate)){
-    aws s3 cp C:\chef\trusted_certs\ip-10-0-1-160_eu-west-2_compute_internal.crt s3://oracle621/chef/ip-10-0-1-160_eu-west-2_compute_internal.crt
+    aws s3 cp $certificate_s3 $certificate_local 
 }
 Write-Output "Completed, chef-server .crtfile placed at C:\chef\trusted_certs."
 
