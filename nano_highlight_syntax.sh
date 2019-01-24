f8b0cc94ea287f3a8b357010c7c68a727d952dd9
root <root@ip-10-0-16-43.eu-west-2.compute.internal>
2019-01-24 16:52:07 +0000

    added comments for nano_highlight_syntax

--- nano_highlight_syntax.sh
+++ nano_highlight_syntax.sh
@@ -1,7 +1,9 @@
-var=`ls /usr/share/nano/`
+var=`ls /usr/share/nano/` # get all nanorc files
 
-arr=($var)
+arr=($var) # put it in an array
 
+# iterate and append to ~/.nanorc file
+# additional string check for only nanorc files
 for i in ${arr[@]};do
   if [[ "$i" =~ nanorc ]]; then
     echo include /usr/share/nano/$i >> ~/.nanorc
